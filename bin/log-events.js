6c7895073ee8f8ad617597e8f17c0ec27a2a1b05
James Nylen <jnylen@gmail.com>
2019-08-21 19:07:56 +0000

    Move logEvent out to a common function

--- bin/log-events.js
+++ bin/log-events.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-const fs   = require( 'fs' );
-const path = require( 'path' );
+const fs = require( 'fs' );
 
 const { RTMClient } = require( '@slack/client' );
 
@@ -11,71 +10,6 @@ const config = lib.loadConfig();
 
 // Confirm write access
 fs.accessSync( config.logPath, fs.constants.W_OK );
-
-let currentLogFile = null;
-let currentHour = null;
-let run = true;
-
-function logEvent( event ) {
-	let friendlyType = event.type;
-	if ( event.subtype ) {
-		friendlyType += '.' + event.subtype;
-	}
-
-	if ( ! run ) {
-		console.error(
-			'Skipping event: %s',
-			friendlyType
-		);
-		return;
-	}
-
-	const lastHour = currentHour;
-	currentHour = new Date().toISOString()
-		.substring( 0, 13 )
-		.replace( /T/, '_' );
-
-	if ( currentHour !== lastHour && currentLogFile ) {
-		console.error(
-			'%s: Closing log',
-			lastHour
-		);
-		currentLogFile.end();
-		currentLogFile = null;
-	}
-
-	if ( ! currentLogFile ) {
-		const currentMonth = currentHour.substring( 0, 7 );
-		console.error(
-			'%s: Opening log',
-			currentHour
-		);
-		try {
-			fs.mkdirSync( path.join( config.logPath, currentMonth ) );
-		} catch ( err ) {
-			if ( err.code !== 'EEXIST' ) {
-				throw err;
-			}
-		}
-		currentLogFile = fs.createWriteStream(
-			path.join( config.logPath, currentMonth, currentHour + '.log' ),
-			{ flags: 'a' }
-		);
-	}
-
-	console.error(
-		'%s: Writing event: %s',
-		currentHour,
-		friendlyType
-	);
-	const ok = currentLogFile.write( JSON.stringify( event ) + '\n' );
-	if ( ! ok ) {
-		console.error(
-			'%s: WARNING: write() returned false!',
-			lastHour
-		);
-	}
-}
 
 // Start listening for Slack events
 const rtm = new RTMClient( config.token );
@@ -90,7 +24,7 @@ rtm.on( 'unable_to_rtm_start', () => {
 } );
 
 rtm.on( 'disconnected', () => {
-	if ( run ) {
+	if ( lib.isLogging() ) {
 		throw new Error( 'Slack: disconnected' );
 	}
 } );
@@ -129,23 +63,18 @@ rtm.on( 'slack_event', ( eventType, event ) => {
 		case 'member_joined_channel':
 		case 'member_left_channel':
 		case 'user_change':
-			logEvent( event );
+			lib.logEvent( config.logPath, event );
 			break;
 	}
 } );
 
 process.on( 'SIGINT', () => {
 	console.error( 'Caught SIGINT; cleaning up' );
-	run = false;
+	lib.stopLogging();
 	rtm.disconnect().then( () => {
-		function exit() {
+		lib.closeLogFile( () => {
 			console.error( 'Exiting' );
 			process.exit( 1 );
-		}
-		if ( currentLogFile ) {
-			currentLogFile.end( exit );
-		} else {
-			exit();
-		}
+		} );
 	} );
 } );
